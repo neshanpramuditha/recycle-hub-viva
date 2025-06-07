52ee6f1d5f7963a06adb7a124774f8e268858ddf
Chathura Sudaraka <Chathurabro68@gmail.com>
2025-06-07 22:39:40 +0530

    Upadte

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import Navigation_bar from "./Navigation_bar.js";
-import Home from "./Home.js"
-import About from "./About.js"
-import Services from "./Services.js"
-import Buy_And_Sale from "./Buy_And_Sale.js"
-import Contact from "./Contact.js"
-import Plastic_Item from "./Plastic_Item.js"
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from "./Home.js";
+import About from "./About.js";
+import Services from "./Services.js";
+import Buy_And_Sale from "./Buy_And_Sale.js";
+import Contact from "./Contact.js";
+import Plastic_Item from "./Plastic_Item.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sale from "./Sale.js";
-import Register from "./Register.js"
+import Register from "./Register.js";
 import Sale_Add_Item from "./Sale_Add_Item.js";
 import Add_Item_form from "./Add_Item_form.js";
 import Buy from "./Buy.js";
@@ -16,43 +16,32 @@ import Glasses from "./Glasses.js";
 import Tyre from "./Tyre.js";
 import Electronic_Device from "./Electronic_Device.js";
 import Iron_Bar from "./Iron_Bar.js";
-import Book from "./Book.js"
+import Book from "./Book.js";
 
-
-
-export default function 
-() {
+export default function App() {
   return (
     <div>
-       <Navigation_bar/>
-     
-       
-
-       <BrowserRouter>
-             <Routes>
-                 <Route path="/" element={<Home/>}/>
-                 <Route path="/About" element={<About/>}/>
-                 <Route path="/Services" element={<Services/>}/>
-                 <Route path="/Buy_and_Sale" element={<Buy_And_Sale/>}/>
-                 <Route path="/Contact" element={<Contact/>}/>
-                 <Route path="/Plastic_Item" element={<Plastic_Item/>}/>
-                 <Route path="/Sale" element={<Sale/>}/>
-                 <Route path="/Register" element={<Register/>}/>
-                 <Route path="/Sale_Add_Item" element={<Sale_Add_Item/>}/>
-                 <Route path="/Add_Item_form" element={<Add_Item_form/>}/>
-                 <Route path="/Buy" element={<Buy/>}/>
-                 <Route path="/Glasses" element={<Glasses/>}/>
-                 <Route path="/Tyre" element={<Tyre/>}/>
-                 <Route path="/Electronic_Device"  element={<Electronic_Device/>}/>
-                 <Route path="/Iron_Bar" element={<Iron_Bar/>}/>
-                 <Route path="/Book" element={<Book/>}/>
-                
-             </Routes>
-       </BrowserRouter> 
-
-        
-       
+      <BrowserRouter>
+        <Navigation_bar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Buy_And_Sale" element={<Buy_And_Sale />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Plastic_Item" element={<Plastic_Item />} />
+          <Route path="/Sale" element={<Sale />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Sale_Add_Item" element={<Sale_Add_Item />} />
+          <Route path="/Add_Item_form" element={<Add_Item_form />} />
+          <Route path="/Buy" element={<Buy />} />
+          <Route path="/Glasses" element={<Glasses />} />
+          <Route path="/Tyre" element={<Tyre />} />
+          <Route path="/Electronic_Device" element={<Electronic_Device />} />
+          <Route path="/Iron_Bar" element={<Iron_Bar />} />
+          <Route path="/Book" element={<Book />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-
--- src/Navigation_bar.js
+++ src/Navigation_bar.js
@@ -1,28 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Navigation_bar.css";
-import About from "./About"
 
 export default function Navigation_bar() {
   return (
     <div>
-      <nav class="navbar navbar-dark bg-success navbar-expand-lg fixed-top ">
-           <a href="" class="navbar-brand" id="Recycle_Hub">Recycle Hub</a>
+      <nav className="navbar navbar-dark bg-success navbar-expand-lg fixed-top">
+        <Link to="/" className="navbar-brand" id="Recycle_Hub">
+          Recycle Hub
+        </Link>
 
-           <button class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#toggle"
+          aria-controls="toggle"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <div class="collapse navbar-collapse justify-content-center" id="toggle" >
-           <ul class="navbar-nav">
-               <li class="nav-item" ><a href="/" class="nav-link active"id="link1">Home</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
-               <li class="nav-item" ><a href="/About" class="nav-link active"id="link2">About</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
-               <li class="nav-item" ><a href="/Services" class="nav-link active"id="link3">Services</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
-               <li class="nav-item" ><a href="/Buy_And_Sale" class="nav-link active"id="link4">buy & sale</a></li>&nbsp;&nbsp;&nbsp;&nbsp;
-               <li class="nav-item"><a href="/Contact" type="button" class=" btn btn-dark nav-link" id="contact_button"><b>CONTACT</b></a></li>
-               
-           </ul>
-          </div>
-       </nav>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="toggle"
+        >
+          <ul className="navbar-nav">
+            <div className="nav-links-center">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active" id="link1">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/About" className="nav-link active" id="link2">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Services" className="nav-link active" id="link3">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Buy_And_Sale" className="nav-link active" id="link4">
+                  Buy & Sale
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Contact" className="nav-link active" id="link5">
+                  Contact
+                </Link>
+              </li>            </div>
+            <li className="nav-item">
+              <Link
+                to="/Register"
+                type="button"
+                className="btn btn-dark nav-link"
+                id="signup_button"
+              >
+                <b>SIGN UP</b>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
-
-
